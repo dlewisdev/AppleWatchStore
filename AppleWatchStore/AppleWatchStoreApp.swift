ef7e361c6909142adbc2e56cbdb72fb2ab6e9fcb
Danielle Lewis <msdaniellelewis@icloud.com>
2023-10-31 15:23:51 -0400

    Initial Commit

---
+++ AppleWatchStore/AppleWatchStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchStoreApp.swift
+//  AppleWatchStore
+//
+//  Created by Danielle Lewis on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
