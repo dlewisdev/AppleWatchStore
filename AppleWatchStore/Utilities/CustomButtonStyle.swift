4335d6c1287c8a9ee97f99bc003afd57c8121163
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 16:44:04 -0400

    Adds reusable custom button style.

--- AppleWatchStore/Utilities/CustomButtonStyle.swift
+++ AppleWatchStore/Utilities/CustomButtonStyle.swift
@@ -5,4 +5,24 @@
 //  Created by Danielle Lewis on 11/2/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomButtonStyle: ButtonStyle {
+    var radius: CGFloat = 4
+    var background: Color = .black
+    var border: Color = .clear
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.horizontal, 5)
+            .padding(.vertical, 8)
+            .background(background)
+            .clipShape(RoundedRectangle(cornerRadius: radius))
+            .buttonStyle(.plain)
+            .overlay(
+                RoundedRectangle(cornerRadius: radius)
+                    .stroke(border, lineWidth: 1)
+            )
+    }
+}
