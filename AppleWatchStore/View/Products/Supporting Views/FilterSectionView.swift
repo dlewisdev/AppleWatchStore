f79adaaaba172828c703493043662c465d22b0d2
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 19:01:56 -0400

    Adds prototype of FilterSectionView.

--- AppleWatchStore/View/Products/Supporting Views/FilterSectionView.swift
+++ AppleWatchStore/View/Products/Supporting Views/FilterSectionView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct FilterSectionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SectionHeader(title: "SECTION TITLE HERE")
+            
+            LazyVGrid(columns: Constants.filterColumns, spacing: 10) {
+                ForEach(0 ..< 5) { item in
+                    sectionItem
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+    
+    var sectionItem: some View {
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 24, height: 24)
+                
+                Image(.checkmark)
+            }
+            
+                Text("Title goes here")
+                    .condensed(.regular, size: 16)
+            Spacer()
+            
+        }
     }
 }
 
