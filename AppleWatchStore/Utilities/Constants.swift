2f3773bcab299c07ee6bdb1386489504649a617f
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 19:02:31 -0400

    Adds filterColumns to contants file for FilterView.

--- AppleWatchStore/Utilities/Constants.swift
+++ AppleWatchStore/Utilities/Constants.swift
@@ -14,6 +14,12 @@ enum Constants {
         GridItem(.flexible(minimum: 100, maximum: .infinity))
     ]
     
+    static let filterColumns = [
+        GridItem(.adaptive(minimum: 120)),
+        GridItem(.adaptive(minimum: 120))
+                 
+    ]
+    
     static let gradient = Gradient(stops: [
         .init(color: Color(.baseGradientTop), location: 0.2),
         .init(color: Color(.baseGradientBottom), location: 0.5)
