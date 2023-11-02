f1bcc4047a8ab81b8424bd00cabf51781836799a
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 18:20:39 -0400

    Adds project files.

---
+++ AppleWatchStore/View/Favorites/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  AppleWatchStore
+//
+//  Created by Danielle Lewis on 11/2/23.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
---
+++ AppleWatchStore/View/Products/ProductsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsView.swift
+//  AppleWatchStore
+//
+//  Created by Danielle Lewis on 11/2/23.
+//
+
+import SwiftUI
+
+struct ProductsView: View {
+    var body: some View {
+        Text("Products View")
+    }
+}
+
+#Preview {
+    ProductsView()
+}
