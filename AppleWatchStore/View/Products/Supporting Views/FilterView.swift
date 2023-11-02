f7280c09c82e2c278bdf6f16fdff49601d29aac1
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 19:01:38 -0400

    Adds prototype of FilterView.

--- AppleWatchStore/View/Products/Supporting Views/FilterView.swift
+++ AppleWatchStore/View/Products/Supporting Views/FilterView.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct FilterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                content
+            }
+            .background(Color(.baseBackground))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    closeButton
+                }
+            }
+            .navigationTitle("PRODUCT FILTERS")
+            .safeAreaInset(edge: .bottom) {
+                filterStatus
+            }
+        }
+    }
+    
+    var content: some View {
+        VStack(spacing: 50) {
+            ForEach(0 ..< 3) { item in
+                FilterSectionView()
+            }
+        }
+        .padding(.top, 40)
+    }
+    
+    var filterStatus: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.white.opacity(0.2))
+                .frame(height: 125)
+                .background(
+                    .ultraThinMaterial,
+                    in: Rectangle()
+                )
+            
+            Button(action: {}) {
+                Text("FILTER (0 ITEMS)")
+                    .condensedLowercased(.medium, size: 24)
+                    .foregroundStyle(.white)
+            }
+            .buttonStyle(.customBorderedBlack)
+            .padding(.horizontal, 20)
+        }
+        .offset(y: 32)
+        .frame(height: 125)
+    }
+    
+    var closeButton: some View {
+        Button(action: {}) {
+            Text("CLOSE")
+                .foregroundStyle(.primary)
+                .condensed(.bold, size: 18)
+        }
+        .buttonStyle(.plain)
     }
 }
 
