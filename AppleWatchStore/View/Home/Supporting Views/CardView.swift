46a6ea64abc964a45ec9b269fe1751c89a432944
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 16:42:22 -0400

    Adds prototype Card View.

--- AppleWatchStore/View/Home/Supporting Views/CardView.swift
+++ AppleWatchStore/View/Home/Supporting Views/CardView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack(alignment: .bottom) {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Constants.gradient)
+                    .frame(height: 245.0 / 1.55)
+                
+                
+                VStack {
+                    Image(.watchCard40Ultra)
+                    
+                    VStack {
+                        VStack(alignment: .center, spacing: -5) {
+                            Text("Apple Watch Ultra")
+                                .condensed(.light, size: 16)
+                            Text("Ready For Adventure")
+                                .condensedLowercased(.bold, size: 24)
+                        }
+                        
+                        Button(action: {}) {
+                            Text("Shop")
+                                .condensedLowercased(.medium, size: 24)
+                                .foregroundStyle(.white)
+                        }
+                        .buttonStyle(.customBorderedBlack)
+                    }
+                    .foregroundStyle(.primary)
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                }
+            }
+        }
     }
 }
 
