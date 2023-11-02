ecfd0b63d557fa59c467d494b3b0dda38ddaff7a
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 16:43:33 -0400

    Adds Text extensions for ultralight and heavy font weights.

--- AppleWatchStore/Utilities/Text+Extension.swift
+++ AppleWatchStore/Utilities/Text+Extension.swift
@@ -5,4 +5,16 @@
 //  Created by Danielle Lewis on 11/2/23.
 //
 
-import Foundation
+import SwiftUI
+
+extension Text {
+    func ultraLight(size: CGFloat = 14) -> Text {
+        let condensed = UIFont.systemFont(ofSize: size, weight: .ultraLight, width: .condensed)
+        return self.font(Font(condensed))
+    }
+    
+    func heavy(size: CGFloat = 60) -> Text {
+        let condensed = UIFont.systemFont(ofSize: size, weight: .heavy, width: .condensed)
+        return self.font(Font(condensed))
+    }
+}
