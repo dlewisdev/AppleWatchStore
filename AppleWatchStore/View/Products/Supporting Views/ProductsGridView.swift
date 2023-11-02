d72371fda5c14e8deb8382135dba2fc9093d417a
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 19:01:24 -0400

    Adds prototype Products Grid View.

--- AppleWatchStore/View/Products/Supporting Views/ProductsGridView.swift
+++ AppleWatchStore/View/Products/Supporting Views/ProductsGridView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct ProductsGridView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            LazyVGrid(columns: Constants.columns, spacing: 16) {
+                ForEach(0 ..< 12) { item in
+                    NavigationLink {} label: {
+                        GridProductItem()
+                    }
+                    .buttonStyle(.plain)
+                }
+            }
+        }
+        .padding(.top, 2)
     }
 }
 
 #Preview {
-    ProductsGridView()
+    NavigationStack {
+        ProductsGridView()
+    }
 }
