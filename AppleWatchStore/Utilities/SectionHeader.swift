dba60e1c7dd39dea7ec21fb008f0263f327a8291
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 16:43:48 -0400

    Adds reusable Section Header.

--- AppleWatchStore/Utilities/SectionHeader.swift
+++ AppleWatchStore/Utilities/SectionHeader.swift
@@ -5,4 +5,29 @@
 //  Created by Danielle Lewis on 11/2/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct SectionHeader: View {
+    var title: String
+    var icon: String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Text(title)
+                    .condensedLowercased(.bold, size: 24)
+                Spacer()
+                if !icon.isEmpty { Image(systemName: icon) }
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        SectionHeader(title: "Popular")
+        SectionHeader(title: "Popular", icon: "slider.horizontal.3")
+    }
+}
