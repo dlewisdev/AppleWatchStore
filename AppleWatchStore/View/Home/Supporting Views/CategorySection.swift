c14ca749522a06a1c41f5a6a3ebae1e3cd60f43f
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-02 16:55:23 -0400

    Adds a category section containing a header and list items.

--- AppleWatchStore/View/Home/Supporting Views/CategorySection.swift
+++ AppleWatchStore/View/Home/Supporting Views/CategorySection.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct CategorySection: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            SectionHeader(title: "Watch Accessories")
+                .padding(.horizontal)
+            
+            VStack {
+                ForEach(0 ..< 5) { item in
+                    CategoryListItem()
+                }
+            }
+        }
     }
 }
 
