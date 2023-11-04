45c570ee866f1863c52fb620dfd5303f16e39284
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-04 13:40:17 -0400

    Adds WideProductItem to project.

---
+++ AppleWatchStore/View/Products/Supporting Views/WideProductItem.swift
@@ -0,0 +1,18 @@
+//
+//  WideProductItem.swift
+//  AppleWatchStore
+//
+//  Created by Danielle Lewis on 11/4/23.
+//
+
+import SwiftUI
+
+struct WideProductItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WideProductItem()
+}
