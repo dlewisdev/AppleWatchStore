a9151260e3f9b6a77e2cbe1cd865126474fc2511
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-04 15:57:35 -0400

    Adds design prototype for the case size view.

--- AppleWatchStore/View/ProductDetail/SupportingViews/CaseSizesView.swift
+++ AppleWatchStore/View/ProductDetail/SupportingViews/CaseSizesView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct CaseSizesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SectionHeader(title: "Case Sizes")
+                .padding(.bottom)
+            
+            ForEach(0 ..< 3) { item in
+                Button {
+                    //action
+                } label: {
+                    SizeItem()
+                        .contentShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.baseMediumGray, lineWidth: 1)
+                        )
+                }
+                .buttonStyle(.plain)
+            }
+        }
+        .padding(.horizontal)
     }
 }
 
