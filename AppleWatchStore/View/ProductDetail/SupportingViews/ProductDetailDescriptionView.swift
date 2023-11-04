a06f267b790b4e195ab40c0c34b6c7d1bd11e721
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-04 14:22:25 -0400

    Adds prototype design for ProductDetailDescriptionView.

--- AppleWatchStore/View/ProductDetail/SupportingViews/ProductDetailDescriptionView.swift
+++ AppleWatchStore/View/ProductDetail/SupportingViews/ProductDetailDescriptionView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct ProductDetailDescriptionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Midnight Aluminum Case with Nike Sports Band")
+                    .condensed(.bold, size: 34)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                
+                HStack(alignment: .center) {
+                    Image(systemName: "star")
+                        .font(.system(size: 16))
+                        .symbolVariant(.fill)
+                        .foregroundStyle(.baseGold)
+                    
+                    Text("5.0")
+                        .condensed(.bold, size: 22)
+                    
+                    Button(action: {})
+                    {
+                        Text("(999 Reviews)")
+                            .condensed(.light, size: 16)
+                    }
+                    Spacer()
+                    Button(action: {})
+                    {
+                        Text("Add Review".uppercased())
+                            .condensed(.light, size: 16)
+                    }
+                }
+                .padding(.bottom, 5)
+                
+                Text("The aluminum case is lightweight and made from 100 percent recycled aerospace-grade alloy. \n\nThe Nike Sport Band is made from a durable high-performance fluorelastomer with compression-molded perforations for breathability.")
+                    .condensed(.light, size: 16)
+            }
+            
+            Spacer()
+        }
     }
 }
 
