733da95c0d2553eb77493d4705e734284ee6e4b7
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-04 15:56:52 -0400

    Adds files for the CaseSize section.

---
+++ AppleWatchStore/View/ProductDetail/SupportingViews/CaseSizesView.swift
@@ -0,0 +1,18 @@
+//
+//  CaseSizesView.swift
+//  AppleWatchStore
+//
+//  Created by Danielle Lewis on 11/4/23.
+//
+
+import SwiftUI
+
+struct CaseSizesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaseSizesView()
+}
