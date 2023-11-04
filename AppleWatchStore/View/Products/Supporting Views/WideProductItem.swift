e1ab182afc7b39993e2dc1b471551b820d0fa891
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-04 13:40:35 -0400

    Adds prototype design of WideProductItem.

--- AppleWatchStore/View/Products/Supporting Views/WideProductItem.swift
+++ AppleWatchStore/View/Products/Supporting Views/WideProductItem.swift
@@ -9,7 +9,55 @@ import SwiftUI
 
 struct WideProductItem: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            HStack(alignment: .bottom, spacing: 0) {
+                VStack(spacing: 5) {
+                    ZStack {
+                        Image(.sportBandProductRedL)
+                            .resizable()
+                            .frame(width: 250, height: 250)
+                        
+                        Image(.aluminumMidnightL)
+                            .resizable()
+                            .frame(width: 250, height: 250)
+                    }
+                    .frame(width: 150)
+                    .padding(.trailing)
+                    
+                    HStack {
+                        ForEach(0..<3) { item in
+                            Circle()
+                                .frame(width: 20, height: 20)
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing, spacing: -4) {
+                    Text("$").ultraLight() + Text("999").heavy()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("Starlight Aluminum Case")
+                            .condensed(.bold, size: 14)
+                        Text("Braided Solo Loop")
+                            .condensed(.light, size: 14)
+                    }
+                }
+                .foregroundStyle(.primary)
+            }
+        }
+        .padding()
+        .frame(height: 220, alignment: .bottom)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Constants.gradient)
+        )
+        .frame(maxWidth: .infinity, alignment: .bottom)
+        .overlay(alignment: .topTrailing) {
+            Constants.heart
+        }
+        .padding(.horizontal)
     }
 }
 
