6f83ec7aac0e1c7c456ed023042cac7fbf2dae79
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-04 13:46:33 -0400

    Completes prototype design of FavoritesView.

--- AppleWatchStore/View/Favorites/FavoritesView.swift
+++ AppleWatchStore/View/Favorites/FavoritesView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct FavoritesView: View {
     var body: some View {
-        Text("Favorites View")
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 70) {
+                    ForEach(0 ..< 3) { item in
+                        NavigationLink {} label: {
+                            WideProductItem()
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+                .padding(.top, 60)
+            }
+            .background(Color.baseBackground)
+            .navigationTitle("MY FAVORITES")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
