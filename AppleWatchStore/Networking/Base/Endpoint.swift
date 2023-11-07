596047751773143df9dd53034b7f141b444f9e46
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-07 13:45:56 -0500

    Sets up reusable endpoint enum and extension for current project.

--- AppleWatchStore/Networking/Base/Endpoint.swift
+++ AppleWatchStore/Networking/Base/Endpoint.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+    var port: Int { get }
+}
+
+extension Endpoint {
+    var scheme: String {
+        return "http"
+    }
+    
+    var host: String {
+        return "12.0.0.1"
+    }
+    
+    var port: Int {
+        return 3000
+    }
+    
+    
+}
